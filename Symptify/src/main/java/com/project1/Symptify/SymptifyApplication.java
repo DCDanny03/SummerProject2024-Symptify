b326c9f1b68374a1c1e622836275c9798faaa22d
Daniel Cao <dcdanny03@gmail.com>
2024-07-09 20:11:50 -0400

    Add files via upload

---
+++ Symptify/src/main/java/com/project1/Symptify/SymptifyApplication.java
@@ -0,0 +1,13 @@
+package com.project1.Symptify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymptifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymptifyApplication.class, args);
+	}
+
+}
---
+++ Symptify/src/test/java/com/project1/Symptify/SymptifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1.Symptify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymptifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
